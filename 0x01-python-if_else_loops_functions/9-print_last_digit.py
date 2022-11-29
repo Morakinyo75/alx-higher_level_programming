da695fdbb8648aabb0ec45418b1336296b047f6b
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-11-29 20:52:24 +0000

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-def print_last_digit(number):
-    '''prints the last digit of a number'''
-    last_digit = abs(number) % 10
-    print(f"{last_digit}", end='')
-    return last_digit
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
